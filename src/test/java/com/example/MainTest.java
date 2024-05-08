08d8711e9c17dfaca12a7dc321957b8e27ec9f0b
Amir Mohammad Khorasani <amirilf@protonmail.com>
2024-05-08 16:53:12 +0330

    Add init test

---
+++ src/test/java/com/example/MainTest.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.junit.Assert;
+
+public class MainTest
+        extends TestCase {
+
+    @org.junit.Test
+    public void testDivision() {
+        assertTrue(true);
+    }
+}
